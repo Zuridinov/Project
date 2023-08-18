fa07ef312eb22d4f465223c4cff67961440a668d
Timur <Zuridinov1504@gmail.com>
2023-08-18 12:51:45 +0200

    012 Practice

--- js/practice.js
+++ js/practice.js
@@ -75,3 +75,51 @@ console.log(`https://someurl.com/${category}/5`);
 const user = "Ivan";
 
 alert(`Привет, ${user}`);
+
+// 009 Операторы в JS
+
+console.log('arr' + " - object");
+console.log(4 + +"5");
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2 + 2 * 2 != 8);
+
+
+const isChecked = false, 
+      isClose = false;
+      
+console.log(isChecked || !isClose);
+
+
+// 012 Практика Ч.1 Начинаем создавать приложение
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+      b = prompt("На сколько оцените его?",""),
+      c = prompt("Один из последних просмотренных фильмов?", ""),
+      d = prompt("На сколько оцените его?","");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
