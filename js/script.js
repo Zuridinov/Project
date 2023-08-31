abff072a9dc328e96497f04113b37e28cd044682
Timur <Zuridinov1504@gmail.com>
2023-08-31 10:46:02 +0200

    Марсианин

--- js/script.js
+++ js/script.js
@@ -1,53 +1,38 @@
 "use strict";
-// 028 Получение элементов со страницы
-const box = document.getElementById('box'),
-      btns = document.getElementsByTagName('button'),  
-      circles = document.getElementsByClassName('circle'),
-      wrapper = document.querySelector('.wrapper'),
-      hearts = wrapper.querySelectorAll('.heart'),
-      oneHeart = wrapper.querySelector('.heart');
-      
 
-// box.style.backgroundColor = 'blue';
-// box.style.width = '500px';
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига Справедливости",
+        "Ла-ла ленд",
+        "Одержимость",
+        "Скотт Пилигрим Против..."
+    ]
+}
 
-box.style.cssText = 'background-color: blue; width: 500px';
+const adv = document.querySelectorAll('.sidebar-2__body img'),
+      poster = document.querySelector('.content__main'), 
+      genre = poster.querySelector('.content__subtitle'),
+      movieList = document.querySelector('.saw__list');
 
-btns[1].style.borderRadius = '100%';
-circles[0].style.backgroundColor = 'red';
-
-// for (let i = 0; i < hearts.length; i++) {
-//     hearts[i].style.backgroundColor = 'blue';
-// }
-hearts.forEach(item => {
-    item.style.backgroundColor = 'blue';
+adv.forEach(item => {
+    item.remove();
 });
+genre.textContent = 'Драма';
 
-const div = document.createElement('div');
-// const text = document.createTextNode('Тут был я');
-div.classList.add('black');
-wrapper.append(div);
-// wrapper.appendChild(div);
+poster.style.backgroundImage = 'url("img/bg.jpeg")';
 
-// wrapper.prepend(div);
+movieList.innerHTML = "";
 
-// hearts[0].before(div);
-// hearts[0].after(div);
+movieDB.movies.sort();
 
-// wrapper.insertBefore(div, hearts[1]);
-
-// circles[0].remove();
-// wrapper.removeChild(hearts[1]);
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+    <li class="saw__links">${i + 1}: ${film}</li>
+    `;
+})
 
 
-hearts[0].replaceWith(circles[0]);
-
-// wrapper.replaceChild(circles[0], hearts[1]);
-
-div.innerHTML = "<h1>Hello World</h1>";
-// div.textContent = "Hello";
-
-div.insertAdjacentHTML("afterend", '<h2>Hello</h2>');
 
 
 
